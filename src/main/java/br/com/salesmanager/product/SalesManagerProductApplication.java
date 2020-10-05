aa006b9c6b2a9770a9478d2306f51ef89308845c
RodrigoLimaM <digo100200@gmail.com>
2020-10-05 18:54:40 -0300

    initializing project

---
+++ src/main/java/br/com/salesmanager/product/SalesManagerProductApplication.java
@@ -0,0 +1,13 @@
+package br.com.salesmanager.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagerProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagerProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/salesmanager/product/SalesManagerProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.salesmanager.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagerProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
